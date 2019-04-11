eff68ef0fdb33401ea6309f280663e5b6de47f6a
Christopher Smith <git@chrstsmth.ca>
2019-04-10 23:33:41 -0600

    Use text struct in vigenere

--- src/cipher/vigenere.rs
+++ src/cipher/vigenere.rs
@@ -10,21 +10,21 @@ impl Vigenere {
 }
 
 impl Cipher for Vigenere {
-	type Key = Vec<Alphabet>;
+	type Key = Text;
 
-	fn encipher(&self, plaintext: &[Alphabet], key: Self::Key) -> Vec<Alphabet>
+	fn encipher(&self, plaintext: Text, key: Self::Key) -> Text
 	{
-		let mut ciphertext = Vec::with_capacity(plaintext.len());
-		for pair in plaintext.iter().zip(key.iter().cycle()) {
-			ciphertext.push(*pair.0 + *pair.1);
+		let mut ciphertext = Text{t: Vec::with_capacity(plaintext.t.len())};
+		for pair in plaintext.t.iter().zip(key.t.iter().cycle()) {
+			ciphertext.t.push(*pair.0 + *pair.1);
 		}
 		ciphertext
 	}
-	fn decipher(&self, ciphertext: &[Alphabet], key: Self::Key) -> Vec<Alphabet>
+	fn decipher(&self, ciphertext: Text, key: Self::Key) -> Text
 	{
-		let mut plaintext = Vec::with_capacity(ciphertext.len());
-		for pair in ciphertext.iter().zip(key.iter().cycle()) {
-			plaintext.push(*pair.0 - *pair.1);
+		let mut plaintext = Text{t: Vec::with_capacity(ciphertext.t.len())};
+		for pair in ciphertext.t.iter().zip(key.t.iter().cycle()) {
+			plaintext.t.push(*pair.0 - *pair.1);
 		}
 		plaintext
 	}
