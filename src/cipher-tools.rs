9209b997995cf5d72c3da759bd7504a71e416f84
Christopher T Smith <git@chrstsmth.ca>
2019-04-19 13:12:38 -0600

    Macros for command parsing & executing

--- src/cipher-tools.rs
+++ src/cipher-tools.rs
@@ -10,50 +10,78 @@ mod pallet;
 use cipher::vigenere::*;
 use cipher::Cipher;
 
+macro_rules! key_arg {
+	() => (
+		Arg::with_name("key")
+			.short("k")
+			.value_name("KEY")
+			.required(true)
+		)
+}
+
+macro_rules! decipher_subcommand {
+	() => (
+		SubCommand::with_name("decipher")
+		.about("Decipher ciphertext")
+		.arg(Arg::with_name("ciphertext")
+			 .short("c")
+			 .value_name("CIPHERTEXT")
+			 .required(true))
+		.arg(key_arg!())
+	)
+}
+
+macro_rules! encipher_subcommand {
+	() => (
+		SubCommand::with_name("encipher")
+		.about("Encipher plaintext")
+		.arg(Arg::with_name("plaintext")
+			 .short("p")
+			 .value_name("PLAINTEXT")
+			 .required(true))
+		.arg(key_arg!())
+	)
+}
+
+macro_rules! encipher {
+	($matches:ident, $Cipher:ident) => (
+		if let Some(matches) = $matches.subcommand_matches("encipher") {
+			let plaintext = String::from(matches.value_of("plaintext").unwrap());
+			let key = $Cipher::parse(matches.value_of("key").unwrap());
+
+			match key {
+				Some(key) => println!("{:}", $Cipher::encipher(plaintext, key)),
+				_ => println!("Parse key failed"),
+			}
+		}
+	)
+}
+
+macro_rules! decipher {
+	($matches:ident, $Cipher:ident) => (
+		if let Some(matches) = $matches.subcommand_matches("decipher") {
+			let ciphertext = String::from(matches.value_of("ciphertext").unwrap());
+			let key = $Cipher::parse(matches.value_of("key").unwrap());
+
+			match key {
+				Some(key) => println!("{:}", $Cipher::encipher(ciphertext, key)),
+				_ => println!("Parse key failed"),
+			}
+		}
+	)
+}
+
 fn main() {
 	let matches = App::new("Cipher Tools")
 		.setting(AppSettings::ArgRequiredElseHelp)
 		.subcommand(SubCommand::with_name("vigenere")
 			.setting(AppSettings::ArgRequiredElseHelp)
-			.subcommand(SubCommand::with_name("encipher")
-				.about("Encipher plaintext")
-				.arg(Arg::with_name("plaintext")
-					.short("p")
-					.value_name("PLAINTEXT")
-					.required(true))
-				.arg(Arg::with_name("key")
-					.short("k")
-					.value_name("KEY")
-					.required(true)))
-			.subcommand(SubCommand::with_name("decipher")
-				.about("Decipher ciphertext")
-				.arg(Arg::with_name("ciphertext")
-					.short("c")
-					.value_name("CIPHERTEXT")
-					.required(true))
-				.arg(Arg::with_name("key")
-					.short("k")
-					.value_name("KEY")
-					.required(true))))
+			.subcommand(encipher_subcommand!())
+			.subcommand(decipher_subcommand!()))
 		.get_matches();
 
 	if let Some(matches) = matches.subcommand_matches("vigenere") {
-		if let Some(matches) = matches.subcommand_matches("encipher") {
-			let plaintext = String::from(matches.value_of("plaintext").unwrap());
-			let key = Vigenere::parse(matches.value_of("key").unwrap());
-
-			match key {
-				Some(key) => println!("{:}", Vigenere::encipher(plaintext, key)),
-				_ => println!("Parse key failed"),
-			}
-		} else if let Some(matches) = matches.subcommand_matches("decipher") {
-			let ciphertext = String::from(matches.value_of("ciphertext").unwrap());
-			let key = Vigenere::parse(matches.value_of("key").unwrap());
-
-			match key {
-				Some(key) => println!("{:}", Vigenere::encipher(ciphertext, key)),
-				_ => println!("Parse key failed"),
-			}
-		}
+		encipher!(matches, Vigenere);
+		decipher!(matches, Vigenere);
 	}
 }
