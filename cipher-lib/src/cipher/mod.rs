fccae5a18d5694377c838d7527b2eded6cd67a57
Christopher Smith <git@chrstsmth.ca>
2019-05-05 20:45:51 -0600

    Add DictionaryAttack trait

--- cipher-lib/src/cipher/mod.rs
+++ cipher-lib/src/cipher/mod.rs
@@ -2,6 +2,8 @@ pub mod vigenere;
 pub mod caesar;
 
 use crate::key::*;
+use crate::candidate::*;
+use crate::language_model::*;
 
 pub trait Cipher {
 	type Key: Key;
@@ -10,3 +12,8 @@ pub trait Cipher {
 	fn decipher(ciphertext: &String, k: &Self::Key) -> String;
 }
 
+pub trait DictionaryAttack<S>: Cipher where
+	S: Iterator<Item = Self::Key>,
+{
+	fn dictionary_attack(ciphertext: String, dictionary: S, n: usize, lang: LanguageModel) -> Vec<Candidate<Self::Key>>;
+}
