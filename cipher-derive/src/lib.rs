f5d369b95bbed9582178d05e3ee433d97d24d850
Christopher T Smith <git@chrstsmth.ca>
2019-05-26 12:27:54 -0600

    first implementation of hill climb

--- cipher-derive/src/lib.rs
+++ cipher-derive/src/lib.rs
@@ -86,3 +86,71 @@ fn impl_brute_force(ast: &syn::DeriveInput) -> TokenStream {
 	};
 	TokenStream::from(expanded)
 }
+
+#[proc_macro_derive(HillClimb)]
+pub fn hill_climb(input: TokenStream) -> TokenStream {
+	let ast = syn::parse_macro_input!(input as DeriveInput);
+	impl_hill_climb(&ast)
+}
+
+fn impl_hill_climb(ast: &syn::DeriveInput) -> TokenStream {
+	let name = &ast.ident;
+	let expanded = quote! {
+		impl<S,M,E> HillClimb<S,M,E> for #name where
+			S: Iterator<Item = Self::Key>,
+			M: FnMut(Candidate<Self>),
+			E: Fn() -> bool,
+		{
+			type MutationKey = Self::Key;
+
+			fn hill_climb(ciphertext: &str, dict: S, lang: LanguageModel, mut candidates: M, exit: E)
+			{
+				for key in dict {
+					let text = #name::decipher(&ciphertext, &key);
+
+					let mut alph_iter = text.chars()
+						.map(|x| Lang::try_from(x))
+						.filter(|x| x.is_ok())
+						.map(|x| x.unwrap());
+
+					let mut best_mutation = Candidate {
+						score: lang.score(&mut alph_iter),
+						text: text,
+						key: key.clone(),
+					};
+					candidates(best_mutation.clone());
+
+					let mut climbed = true;
+					while climbed {
+						climbed = false;
+
+						for mutated_key in key.clone().into_mutation_iterator() {
+							let text = #name::decipher(&ciphertext, &mutated_key);
+							let mut alph_iter = text.chars()
+								.map(|x| Lang::try_from(x))
+								.filter(|x| x.is_ok())
+								.map(|x| x.unwrap());
+
+							let competitor = Candidate {
+								score: lang.score(&mut alph_iter),
+								text: text,
+								key: mutated_key.clone(),
+							};
+							if competitor > best_mutation {
+								best_mutation = competitor;
+								climbed = true;
+							}
+
+							candidates(best_mutation.clone());
+
+							if exit() {
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+	};
+	TokenStream::from(expanded)
+}
