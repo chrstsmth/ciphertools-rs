0effe78a8e356ce55ae6ae3fe404f3e63f79f122
Christopher T Smith <git@chrstsmth.ca>
2019-05-26 23:58:43 -0600

    Remove score from LanguageModel

---
+++ cipher-lib/src/score.rs
@@ -0,0 +1,29 @@
+use std::convert::TryFrom;
+
+use crate::language_model::*;
+use crate::pallet::lang::*;
+
+pub fn score<S>(tr: LanguageModelTraverser, mut s: S) -> u32 where
+	S: Iterator<Item = Lang> + Clone,
+{
+	let mut score: u32 = 0;
+	loop {
+		let text = s.clone();
+		let mut cursor = tr.clone();
+
+		for (i, c) in text.enumerate() {
+			let next = cursor.next(c);
+			match next {
+				None => break,
+				Some(node) => {
+					score += (u32::try_from(i).unwrap() + 1) * node.freq();
+				}
+			}
+		}
+
+		if s.next().is_none() {
+			break;
+		}
+	}
+	score
+}
