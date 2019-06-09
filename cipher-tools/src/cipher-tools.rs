cc7bc7afc71bf0ae4dcfdbfa3c7f023dd9e601a8
Christopher Smith <ciphertools@christsmith.ca>
2019-06-08 22:07:54 -0600

    Refactor cipher-tools.rs

--- cipher-tools/src/cipher-tools.rs
+++ cipher-tools/src/cipher-tools.rs
@@ -4,12 +4,13 @@ extern crate serde_json;
 extern crate clap;
 extern crate cipher_lib;
 
-use clap::{Arg, App, SubCommand, AppSettings, ArgMatches};
+use clap::{Arg, ArgGroup, App, SubCommand, AppSettings, ArgMatches};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::*;
+use std::io;
 use std::error::Error;
 use std::process;
 use std::str::FromStr;
@@ -26,207 +27,220 @@ use cipher_lib::language_model::*;
 use cipher_lib::pallet::lang::*;
 use cipher_lib::score::*;
 
-fn key_arg<'a,'b>() -> Arg<'a,'b> {
-	Arg::with_name("key")
-		.short("k")
-		.value_name("KEY")
-		.required(true)
-}
 
-fn ciphertext_arg<'a,'b>() -> Arg<'a,'b>
-{
-	Arg::with_name("ciphertext")
-		.short("c")
-		.value_name("CIPHERTEXT")
-		.required(true)
-}
 
-fn plaintext_arg<'a,'b>() -> Arg<'a,'b>
-{
-	Arg::with_name("plaintext")
-		.short("p")
-		.value_name("PLAINTEXT")
-		.required(true)
-}
+mod arg {
+	use super::*;
 
-fn language_model_arg<'a,'b>() -> Arg<'a,'b>
-{
-	Arg::with_name("language")
-		.short("l")
-		.value_name("LANGUAGE")
-		.required(true)
-}
+	pub fn key<'a,'b>() -> Arg<'a,'b> {
+		Arg::with_name("key")
+			.short("k")
+			.value_name("KEY")
+			.required(true)
+	}
 
-fn decipher_subcommand<'a,'b>() -> App<'a,'b>
-{
-	SubCommand::with_name("decipher")
-		.about("Decipher ciphertext")
-		.arg(ciphertext_arg())
-		.arg(key_arg())
-}
+	pub fn ciphertext<'a,'b>() -> Arg<'a,'b>
+	{
+		Arg::with_name("ciphertext")
+			.short("c")
+			.value_name("CIPHERTEXT")
+			.required(true)
+	}
 
-fn encipher_subcommand<'a,'b>() -> App<'a,'b>
-{
-	SubCommand::with_name("encipher")
-		.about("Encipher plaintext")
-		.arg(plaintext_arg())
-		.arg(key_arg())
-}
+	pub fn plaintext<'a,'b>() -> Arg<'a,'b>
+	{
+		Arg::with_name("plaintext")
+			.short("p")
+			.value_name("PLAINTEXT")
+			.required(true)
+	}
 
-fn dictionary_attack_subcommand<'a,'b>() -> App<'a,'b>
-{
-	SubCommand::with_name("dictionary")
-		.about("Dictionary attack")
-		.arg(ciphertext_arg())
-		.arg(language_model_arg())
-		.arg(Arg::with_name("dictionary")
+	pub fn language_model<'a,'b>() -> Arg<'a,'b>
+	{
+		Arg::with_name("language")
+			.short("l")
+			.value_name("LANGUAGE")
+			.required(true)
+	}
+
+	pub fn dictionary<'a,'b>() -> Arg<'a,'b>
+	{
+		Arg::with_name("dictionary")
 			.short("d")
 			.value_name("DICTIONARY")
-			.required(true))
+			.required(true)
+	}
 }
 
-fn brute_force_subcommand <'a,'b>() -> App<'a,'b>
-{
-	SubCommand::with_name("brute")
-		.about("Brute force")
-		.arg(ciphertext_arg())
-		.arg(language_model_arg())
-		.arg(Arg::with_name("start")
-			.short("s")
-			.value_name("START-KEY")
-			.required(false))
-		.arg(Arg::with_name("end")
-			.short("e")
-			.value_name("END-KEY")
-			.required(false))
+mod subcommand {
+	use super::*;
+
+	pub fn dictionary_attack<'a,'b>() -> App<'a,'b>
+	{
+		SubCommand::with_name("dictionary")
+			.about("Dictionary attack")
+			.arg(arg::ciphertext())
+			.arg(arg::language_model())
+			.arg(arg::dictionary())
+	}
+
+	pub fn decipher<'a,'b>() -> App<'a,'b>
+	{
+		SubCommand::with_name("decipher")
+			.about("Decipher ciphertext")
+			.arg(arg::ciphertext())
+			.arg(arg::key())
+	}
+
+	pub fn encipher<'a,'b>() -> App<'a,'b>
+	{
+		SubCommand::with_name("encipher")
+			.about("Encipher plaintext")
+			.arg(arg::plaintext())
+			.arg(arg::key())
+	}
+
+	pub fn brute_force<'a,'b>() -> App<'a,'b>
+	{
+		SubCommand::with_name("brute") .about("Brute force") .arg(arg::ciphertext()) .arg(arg::language_model())
+			.arg(Arg::with_name("start")
+				.short("s")
+				.value_name("START-KEY")
+				.required(false))
+			.arg(Arg::with_name("end")
+				.short("e")
+				.value_name("END-KEY")
+				.required(false))
+	}
+
+	pub fn hill_climb<'a,'b>() -> App<'a,'b>
+	{
+		SubCommand::with_name("hill")
+			.about("Hill climb")
+			.arg(arg::ciphertext())
+			.arg(arg::language_model())
+			.arg(arg::dictionary())
+	}
 }
 
-fn hill_climb_subcommand<'a,'b>() -> App<'a,'b>
-{
-	SubCommand::with_name("hill")
-		.about("Hill climb")
-		.arg(ciphertext_arg())
-		.arg(language_model_arg())
-		.arg(Arg::with_name("dictionary")
-			.short("d")
-			.value_name("DICTIONARY")
-			.required(true))
-}
+mod parse {
+	use super::*;
 
-fn parse_language_model_arg<'a>(matches: &ArgMatches<'a>) -> LanguageModel
-{
-	let filename = String::from(matches.value_of("language").unwrap());
+	pub fn language_model_parse<'a>(matches: &ArgMatches<'a>) -> LanguageModel
+	{
+		let filename = String::from(matches.value_of("language").unwrap());
 
-	let file = match File::open(&filename) {
-		Err(why) => {
-			eprintln!("{}: {}", filename, why);
-			process::exit(1);
-		}
-		Ok(file) => file,
-	};
-
-	let language = match serde_json::from_reader(BufReader::new(file)) {
-		Err(why) => {
-			eprintln!("{}: {}", filename, why);
-			process::exit(1);
-		}
-		Ok(language) => language,
-	};
-
-	language
-}
-
-fn parse_dictionary_arg<'a, C>(matches: &ArgMatches<'a>) -> impl Iterator<Item = C::Key> where
-	C: Cipher,
-{
-	let filename = String::from(matches.value_of("dictionary").unwrap());
-
-	let file = match File::open(&filename) {
-		Err(why) => {
-			eprintln!("{}: {}", filename, why);
-			process::exit(1);
-		}
-		Ok(file) => file,
-	};
-
-	let dict = BufReader::new(file)
-		.lines()
-		.map(|x| x.unwrap_or_else(|e|
-			{
-				eprintln!("{}", e.description());
+		let file = match File::open(&filename) {
+			Err(why) => {
+				eprintln!("{}: {}", filename, why);
 				process::exit(1);
-			})
-			)
-		.enumerate()
-		.map(move |x| {
-			let (num, line) = x;
-			let line_num = num + 1;
-			match C::Key::from_str(line.as_str()) {
-				Err(_) => {
-					eprintln!("{}:{}: failed to parse \"{}\"", filename, line_num, line);
-					process::exit(1);
-				}
-				Ok(key) => key
 			}
-			});
+			Ok(file) => file,
+		};
 
-	dict
-}
+		let language = match serde_json::from_reader(BufReader::new(file)) {
+			Err(why) => {
+				eprintln!("{}: {}", filename, why);
+				process::exit(1);
+			}
+			Ok(language) => language,
+		};
 
-fn parse_key_arg<'a, C: Cipher>(matches: &ArgMatches<'a>) -> C::Key
-{
-	let key_str = matches.value_of("key").unwrap();
-	let key = C::Key::from_str(key_str);
-
-	match key {
-		Ok(key) => key,
-		_ => {
-			println!("{}: Parse key failed", key_str);
-			process::exit(1);
-		}
-	}
-}
-
-fn parse_text_arg<'a>(matches: &ArgMatches<'a>, text: &str) -> String
-{
-	let filename = String::from(matches.value_of(text).unwrap());
-
-	let file = match File::open(&filename) {
-		Err(why) => {
-			eprintln!("{}: {}", filename, why);
-			process::exit(1);
-		}
-		Ok(file) => file,
-	};
-
-	let mut reader = BufReader::new(file);
-
-	let mut text = String::new();
-	match reader.read_to_string(&mut text) {
-		Err(why) => {
-			eprintln!("{}: {}", filename, why);
-		},
-		Ok(_) => (),
+		language
 	}
 
-	text
-}
+	pub fn dictionary_parse<'a, C>(matches: &ArgMatches<'a>) -> impl Iterator<Item = C::Key> where
+		C: Cipher,
+	{
+		let filename = String::from(matches.value_of("dictionary").unwrap());
 
-fn parse_ciphertext_arg<'a>(matches: &ArgMatches<'a>) -> String
-{
-	parse_text_arg(matches, "ciphertext")
-}
+		let file = match File::open(&filename) {
+			Err(why) => {
+				eprintln!("{}: {}", filename, why);
+				process::exit(1);
+			}
+			Ok(file) => file,
+		};
 
-fn parse_plaintext_arg<'a>(matches: &ArgMatches<'a>) -> String
-{
-	parse_text_arg(matches, "plaintext")
+		let dict = BufReader::new(file)
+			.lines()
+			.map(|x| x.unwrap_or_else(|e|
+				{
+					eprintln!("{}", e.description());
+					process::exit(1);
+				})
+				)
+			.enumerate()
+			.map(move |x| {
+				let (num, line) = x;
+				let line_num = num + 1;
+				match C::Key::from_str(line.as_str()) {
+					Err(_) => {
+						eprintln!("{}:{}: failed to parse \"{}\"", filename, line_num, line);
+						process::exit(1);
+					}
+					Ok(key) => key
+				}
+				});
+
+		dict
+	}
+
+	pub fn key_parse<'a, C: Cipher>(matches: &ArgMatches<'a>) -> C::Key
+	{
+		let key_str = matches.value_of("key").unwrap();
+		let key = C::Key::from_str(key_str);
+
+		match key {
+			Ok(key) => key,
+			_ => {
+				println!("{}: Parse key failed", key_str);
+				process::exit(1);
+			}
+		}
+	}
+
+	pub fn ciphertext_parse<'a>(matches: &ArgMatches<'a>) -> String
+	{
+		text_parse(matches, "ciphertext")
+	}
+
+	pub fn plaintext_parse<'a>(matches: &ArgMatches<'a>) -> String
+	{
+		text_parse(matches, "plaintext")
+	}
+
+	fn text_parse<'a>(matches: &ArgMatches<'a>, text: &str) -> String
+	{
+		let filename = String::from(matches.value_of(text).unwrap());
+
+		let file = match File::open(&filename) {
+			Err(why) => {
+				eprintln!("{}: {}", filename, why);
+				process::exit(1);
+			}
+			Ok(file) => file,
+		};
+
+		let mut reader = BufReader::new(file);
+
+		let mut text = String::new();
+		match reader.read_to_string(&mut text) {
+			Err(why) => {
+				eprintln!("{}: {}", filename, why);
+			},
+			Ok(_) => (),
+		}
+
+		text
+	}
 }
 
 macro_rules! encipher {
 	($matches:ident, $Cipher:ident) => (
 		if let Some(matches) = $matches.subcommand_matches("encipher") {
-			let plaintext = parse_plaintext_arg(matches);
-			let key = parse_key_arg::<$Cipher>(matches);
+			let plaintext = parse::plaintext_parse(matches);
+			let key = parse::key_parse::<$Cipher>(matches);
 
 			println!("{:}", $Cipher::encipher(&plaintext, &key));
 		}
@@ -236,8 +250,8 @@ macro_rules! encipher {
 macro_rules! decipher {
 	($matches:ident, $Cipher:ident) => (
 		if let Some(matches) = $matches.subcommand_matches("decipher") {
-			let ciphertext = parse_ciphertext_arg(matches);
-			let key = parse_key_arg::<$Cipher>(matches);
+			let ciphertext = parse::ciphertext_parse(matches);
+			let key = parse::key_parse::<$Cipher>(matches);
 
 			println!("{:}", $Cipher::decipher(&ciphertext, &key));
 		}
@@ -247,9 +261,9 @@ macro_rules! decipher {
 macro_rules! dictionary_attack {
 	($matches:ident, $Cipher:ident, $exit:ident) => (
 		if let Some(matches) = $matches.subcommand_matches("dictionary") {
-			let ciphertext = parse_ciphertext_arg(matches);
-			let language  = parse_language_model_arg(matches);
-			let dictionary = parse_dictionary_arg::<$Cipher>(matches);
+			let ciphertext = parse::ciphertext_parse(matches);
+			let language  = parse::language_model_parse(matches);
+			let dictionary = parse::dictionary_parse::<$Cipher>(matches);
 
 			let mut candidates = Candidates::<$Cipher>::with_capacity(10);
 			let insert_candidate = |c: &Candidate<$Cipher>| {
@@ -284,8 +298,8 @@ macro_rules! brute_force {
 			type BruteForceIter = <<$Cipher as Cipher>::Key as IntoBruteForceIterator>::BruteForceIter;
 			type Key = <$Cipher as Cipher>::Key;
 
-			let ciphertext = parse_ciphertext_arg(matches);
-			let language  = parse_language_model_arg(matches);
+			let ciphertext = parse::ciphertext_parse(matches);
+			let language  = parse::language_model_parse(matches);
 
 			let start = match matches.value_of("start") {
 				Some(key_str) => {
@@ -351,9 +365,9 @@ macro_rules! brute_force {
 macro_rules! hill_climb {
 	($matches:ident, $Cipher:ident, $exit:ident) => (
 		if let Some(matches) = $matches.subcommand_matches("hill") {
-			let ciphertext = parse_ciphertext_arg(matches);
-			let language = parse_language_model_arg(matches);
-			let dictionary = parse_dictionary_arg::<$Cipher>(matches);
+			let ciphertext = parse::ciphertext_parse(matches);
+			let language = parse::language_model_parse(matches);
+			let dictionary = parse::dictionary_parse::<$Cipher>(matches);
 
 			let mut candidates = Candidates::<$Cipher>::with_capacity(10);
 			let insert_candidate = |c: &Candidate<$Cipher>| {
@@ -382,6 +396,26 @@ macro_rules! hill_climb {
 	)
 }
 
+fn vigenere_subcommand<'a,'b>() -> App<'a,'b>
+{
+	SubCommand::with_name(Vigenere::NAME)
+		.setting(AppSettings::ArgRequiredElseHelp)
+		.subcommand(subcommand::encipher())
+		.subcommand(subcommand::decipher())
+		.subcommand(subcommand::dictionary_attack())
+		.subcommand(subcommand::brute_force())
+		.subcommand(subcommand::hill_climb())
+}
+
+fn caesar_subcommand<'a,'b>() -> App<'a,'b>
+{
+	SubCommand::with_name(Caesar::NAME)
+		.setting(AppSettings::ArgRequiredElseHelp)
+		.subcommand(subcommand::encipher())
+		.subcommand(subcommand::decipher())
+		.subcommand(subcommand::brute_force())
+}
+
 fn main() {
 	let exit = Arc::new(AtomicBool::new(false));
 	let ctrlc_exit = exit.clone();
@@ -391,19 +425,8 @@ fn main() {
 
 	let matches = App::new("Cipher Tools")
 		.setting(AppSettings::ArgRequiredElseHelp)
-		.subcommand(SubCommand::with_name(Vigenere::NAME)
-			.setting(AppSettings::ArgRequiredElseHelp)
-			.subcommand(encipher_subcommand())
-			.subcommand(decipher_subcommand())
-			.subcommand(dictionary_attack_subcommand())
-			.subcommand(brute_force_subcommand())
-			.subcommand(hill_climb_subcommand()))
-
-		.subcommand(SubCommand::with_name(Caesar::NAME)
-			.setting(AppSettings::ArgRequiredElseHelp)
-			.subcommand(encipher_subcommand())
-			.subcommand(decipher_subcommand())
-			.subcommand(brute_force_subcommand()))
+		.subcommand(vigenere_subcommand())
+		.subcommand(caesar_subcommand())
 		.get_matches();
 
 	if let Some(matches) = matches.subcommand_matches(Vigenere::NAME) {
