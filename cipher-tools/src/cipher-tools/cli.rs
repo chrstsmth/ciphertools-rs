a91b9b5d7221370dd23ce8d3d8731fed89a5b8b2
Christopher Smith <git@chrstsmth.ca>
2019-07-29 22:34:58 -0600

    refactor command line arguments

--- cipher-tools/src/cipher-tools/cli.rs
+++ cipher-tools/src/cipher-tools/cli.rs
@@ -1,122 +1,107 @@
-use clap::{Arg, App, SubCommand, AppSettings};
+use clap::{Arg, ArgGroup, App, SubCommand, AppSettings};
 
 use cipher_lib::cipher::*;
 use cipher_lib::cipher::vigenere::*;
 use cipher_lib::cipher::caesar::*;
 
+fn key_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("key")
+		.short("k")
+		.value_name("KEY")
+}
+
+fn ciphertext_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("ciphertext")
+		.short("c")
+		.value_name("CIPHERTEXT")
+}
+
+fn plaintext_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("plaintext")
+		.short("p")
+		.value_name("PLAINTEXT")
+}
+fn language_model_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("language")
+		.short("l")
+		.value_name("LANGUAGE")
+}
+
+fn dict_file_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("dict_file")
+		.short("dict_file")
+		.value_name("FILE")
+}
+
+fn dict_random_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("dict_random")
+		.long("dict_random")
+}
+
+fn dict_range_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("dict_range")
+		.long("dict_range")
+		.value_name("(START, END)")
+}
+
+fn dict_brute_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("dict_brute")
+		.long("dict_brute")
+}
+
+fn dict_stdin_arg<'a,'b>() -> Arg<'a,'b> {
+	Arg::with_name("dict_stdin")
+		.long("dict_stdin")
+}
+
+fn dictionary_attack_subcommand<'a,'b, C>() -> App<'a,'b>
+where
+	C: DictionaryAttack
+{
+	SubCommand::with_name("dictionary")
+		.about("Dictionary attack")
+		.arg(ciphertext_arg().required(true))
+		.arg(language_model_arg().required(true))
+		.arg(dict_file_arg().required(true))
+}
+
+fn hill_climb_subcommand<'a,'b, C>() -> App<'a,'b>
+where
+	C: HillClimb
+{
+	SubCommand::with_name("hill")
+		.about("Hill climb")
+		.arg(ciphertext_arg().required(true))
+		.arg(language_model_arg().required(true))
+		.arg(dict_file_arg().required(true))
+}
+
+fn decipher_subcommand<'a,'b, C>() -> App<'a,'b>
+where
+	C: Cipher
+{
+	SubCommand::with_name("decipher")
+		.about("Decipher ciphertext")
+		.arg(ciphertext_arg().required(true))
+		.arg(key_arg().required(true))
+}
+
+fn encipher_subcommand<'a,'b, C>() -> App<'a,'b>
+where
+	C: Cipher
+{
+	SubCommand::with_name("encipher")
+		.about("Encipher plaintext")
+		.arg(plaintext_arg().required(true))
+		.arg(key_arg().required(true))
+}
+
+pub trait DictionaryArgs {
+	fn dict_args<'a,'b>() -> Arg<'a,'b>;
+	fn dict_group<'a>() -> ArgGroup<'a>;
+}
+
 pub trait Cli {
-	fn key_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("key")
-			.short("k")
-			.value_name("KEY")
-			.required(true)
-	}
-
-	fn ciphertext_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("ciphertext")
-			.short("c")
-			.value_name("CIPHERTEXT")
-			.required(true)
-	}
-
-	fn plaintext_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("plaintext")
-			.short("p")
-			.value_name("PLAINTEXT")
-			.required(true)
-	}
-	fn language_model_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("language")
-			.short("l")
-			.value_name("LANGUAGE")
-			.required(true)
-	}
-
-	fn dict_file_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("dict_file")
-			.short("dict_file")
-			.value_name("FILE")
-			.required(true)
-	}
-
-	fn dict_random_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("dict_random")
-			.long("dict_random")
-			.required(true)
-	}
-
-	fn dict_range_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("dict_range")
-			.long("dict_range")
-			.value_name("(START, END)")
-			.required(true)
-	}
-
-	fn dict_brute_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("dict_brute")
-			.long("dict_brute")
-			.required(true)
-	}
-
-	fn dict_stdin_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("dict_stdin")
-			.long("dict_stdin")
-			.required(true)
-	}
-
-	fn start_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("start")
-			.short("s")
-			.value_name("START-KEY")
-			.required(true)
-	}
-
-	fn end_arg<'a,'b>() -> Arg<'a,'b> {
-		Arg::with_name("end")
-			.short("e")
-			.value_name("END-KEY")
-			.required(true)
-	}
-
-	fn dictionary_attack_subcommand<'a,'b>() -> App<'a,'b> {
-		SubCommand::with_name("dictionary")
-			.about("Dictionary attack")
-			.arg(Self::ciphertext_arg())
-			.arg(Self::language_model_arg())
-			.arg(Self::dict_file_arg())
-	}
-
-	fn decipher_subcommand<'a,'b>() -> App<'a,'b> {
-		SubCommand::with_name("decipher")
-			.about("Decipher ciphertext")
-			.arg(Self::ciphertext_arg())
-			.arg(Self::key_arg())
-	}
-
-	fn encipher_subcommand<'a,'b>() -> App<'a,'b> {
-		SubCommand::with_name("encipher")
-			.about("Encipher plaintext")
-			.arg(Self::plaintext_arg())
-			.arg(Self::key_arg())
-	}
-
-	fn brute_force_subcommand<'a,'b>() -> App<'a,'b> {
-		SubCommand::with_name("brute")
-			.about("Brute force")
-			.arg(Self::ciphertext_arg())
-			.arg(Self::language_model_arg())
-			.arg(Self::start_arg().required(false))
-			.arg(Self::end_arg().required(false))
-	}
-
-	fn hill_climb_subcommand<'a,'b>() -> App<'a,'b> {
-		SubCommand::with_name("hill")
-			.about("Hill climb")
-			.arg(Self::ciphertext_arg())
-			.arg(Self::language_model_arg())
-			.arg(Self::dict_file_arg())
-	}
-
 	fn command<'a,'b>() -> App<'a,'b>;
 }
 
@@ -124,9 +109,8 @@ impl Cli for Caesar {
 	fn command<'a,'b>() -> App<'a,'b> {
 		SubCommand::with_name(Caesar::NAME)
 			.setting(AppSettings::ArgRequiredElseHelp)
-			.subcommand(Self::encipher_subcommand())
-			.subcommand(Self::decipher_subcommand())
-			.subcommand(Self::brute_force_subcommand())
+			.subcommand(encipher_subcommand::<Self>())
+			.subcommand(decipher_subcommand::<Self>())
 	}
 }
 
@@ -135,11 +119,10 @@ impl Cli for Vigenere {
 	{
 		SubCommand::with_name(Vigenere::NAME)
 			.setting(AppSettings::ArgRequiredElseHelp)
-			.subcommand(Self::encipher_subcommand())
-			.subcommand(Self::decipher_subcommand())
-			.subcommand(Self::dictionary_attack_subcommand())
-			.subcommand(Self::brute_force_subcommand())
-			.subcommand(Self::hill_climb_subcommand())
+			.subcommand(encipher_subcommand::<Self>())
+			.subcommand(decipher_subcommand::<Self>())
+			.subcommand(dictionary_attack_subcommand::<Self>())
+			.subcommand(hill_climb_subcommand::<Self>())
 	}
 }
 
