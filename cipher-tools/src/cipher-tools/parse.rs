a1134a5994823184831a8ab70d3b88cb3bc2df3e
Christopher Smith <git@chrstsmth.ca>
2019-08-08 22:59:54 -0600

    Dedicated parser for each option

--- cipher-tools/src/cipher-tools/parse.rs
+++ cipher-tools/src/cipher-tools/parse.rs
@@ -1,94 +1,90 @@
-use clap::ArgMatches;
 use std::fs::File;
 use std::process;
 
 use cipher_lib::language_model::*;
 use cipher_lib::cipher::*;
+use cipher_lib::cipher::caesar::*;
+use cipher_lib::cipher::vigenere::*;
 use std::io::prelude::*;
 use std::io::*;
 use std::error::Error;
 use std::str::FromStr;
 
-pub struct Arguments<C: Cipher> {
-	pub language_model: Option<LanguageModel>,
-	pub dictionary: Option<Box<dyn Iterator<Item = C::Key>>>,
-	pub ciphertext: Option<String>,
-	pub plaintext: Option<String>,
-	pub key: Option<C::Key>,
-	pub start: Option<C::Key>,
-	pub end: Option<C::Key>,
+pub trait DictionaryOption: Cipher {
+	fn dictionary_option(matches: &clap::ArgMatches) -> Box<dyn Iterator<Item = Self::Key>>;
 }
 
-impl<C: Cipher> Arguments<C> {
-	fn new() -> Arguments<C> {
-		Arguments {
-			language_model: None,
-			dictionary: None,
-			ciphertext: None,
-			plaintext: None,
-			key: None,
-			start: None,
-			end: None,
-		}
+impl DictionaryOption for Caesar {
+	fn dictionary_option(matches: &clap::ArgMatches) -> Box<dyn Iterator<Item = Self::Key>> {
+		let dict_file = matches.value_of("dict-file").unwrap();
+		dictionary_file::<Self>(&dict_file)
 	}
 }
 
-pub fn parse_available<'a, C: Cipher>(matches: &ArgMatches<'a>) -> Arguments<C> {
-	let mut a = Arguments::<C>::new();
+impl DictionaryOption for Vigenere {
+	fn dictionary_option(matches: &clap::ArgMatches) -> Box<dyn Iterator<Item = Self::Key>> {
+		let dict_file = matches.value_of("dict-file").unwrap();
+		dictionary_file::<Self>(&dict_file)
+	}
+}
 
-	a.language_model = match matches.value_of("language") {
-		None => None,
-		Some(filename) => {
-			Some(language_model(filename))
+pub fn language_model_option(matches: &clap::ArgMatches) -> LanguageModel
+{
+	language_model(matches.value_of("language").unwrap())
+}
+
+pub fn ciphertext_option(matches: &clap::ArgMatches) -> String
+{
+	text(matches.value_of("ciphertext").unwrap())
+}
+
+pub fn plaintext_option(matches: &clap::ArgMatches) -> String
+{
+	text(matches.value_of("plaintext").unwrap())
+}
+
+pub fn key_option<C: Cipher>(matches: &clap::ArgMatches) -> C::Key
+{
+	key::<C>(matches.value_of("key").unwrap())
+}
+
+pub fn dictionary_file<C: Cipher>(filename: &str) -> Box<dyn Iterator<Item = C::Key>>
+{
+
+	let file = match File::open(filename) {
+		Err(why) => {
+			eprintln!("{}: {}", filename, why);
+			process::exit(1);
 		}
+		Ok(file) => file,
 	};
 
-	a.dictionary = match matches.value_of("dict-file") {
-		None => None,
-		Some(filename) => {
-			//TODO string conversion shouldn't be necessary, but lifetimes get
-			//a bit complicated with &str
-			//https://users.rust-lang.org/t/box-lifetime-problem/9030
-			Some(dictionary::<C>(String::from(filename)))
-		}
-	};
+	//Must explicitly clone into closure
+	//https://github.com/rust-lang/rfcs/issues/2407
+	let filename_clone = String::from(filename);
 
-	a.ciphertext = match matches.value_of("ciphertext") {
-		None => None,
-		Some(filename) => {
-			Some(text(filename))
-		}
-	};
+	let dict = BufReader::new(file)
+		.lines()
+		.map(|x| x.unwrap_or_else(|e|
+			{
+				eprintln!("{}", e.description());
+				process::exit(1);
+			})
+			)
+		.enumerate()
+		.map(move |x| {
+			let (num, line) = x;
+			let line_num = num + 1;
+			match C::Key::from_str(line.as_str()) {
+				Err(_) => {
+					eprintln!("{}:{}: failed to parse \"{}\"", filename_clone, line_num, line);
+					process::exit(1);
+				}
+				Ok(key) => key
+			}
+			});
 
-	a.plaintext = match matches.value_of("plaintext") {
-		None => None,
-		Some(filename) => {
-			Some(text(filename))
-		}
-	};
-
-	a.key = match matches.value_of("key") {
-		None => None,
-		Some(key_str) => {
-			Some(key::<C>(key_str))
-		}
-	};
-
-	a.start = match matches.value_of("start") {
-		None => None,
-		Some(key_str) => {
-			Some(key::<C>(key_str))
-		}
-	};
-
-	a.end = match matches.value_of("end") {
-		None => None,
-		Some(key_str) => {
-			Some(key::<C>(key_str))
-		}
-	};
-
-	a
+	Box::new(dict)
 }
 
 pub fn language_model(filename: &str) -> LanguageModel
@@ -110,41 +106,6 @@ pub fn language_model(filename: &str) -> LanguageModel
 	};
 
 	language
-}
-
-pub fn dictionary<C>(filename: String) -> Box<dyn Iterator<Item = C::Key>> where
-	C: Cipher,
-{
-	let file = match File::open(&filename) {
-		Err(why) => {
-			eprintln!("{}: {}", filename, why);
-			process::exit(1);
-		}
-		Ok(file) => file,
-	};
-
-	let dict = BufReader::new(file)
-		.lines()
-		.map(|x| x.unwrap_or_else(|e|
-			{
-				eprintln!("{}", e.description());
-				process::exit(1);
-			})
-			)
-		.enumerate()
-		.map(move |x| {
-			let (num, line) = x;
-			let line_num = num + 1;
-			match C::Key::from_str(line.as_str()) {
-				Err(_) => {
-					eprintln!("{}:{}: failed to parse \"{}\"", filename, line_num, line);
-					process::exit(1);
-				}
-				Ok(key) => key
-			}
-			});
-
-	Box::new(dict)
 }
 
 pub fn key<'a, C: Cipher>(key_str: &str) -> C::Key
