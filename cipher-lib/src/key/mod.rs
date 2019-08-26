41b44e03bb08ae7cb4c83d75447b3112d48cde27
Christopher Smith <git@chrstsmth.ca>
2019-08-26 13:32:29 -0600

    Specify key iterator associated item type

--- cipher-lib/src/key/mod.rs
+++ cipher-lib/src/key/mod.rs
@@ -10,20 +10,20 @@ pub trait Key: FromStr + fmt::Display + Clone + Eq + Ord {
 }
 
 pub trait IntoBruteForceIterator: Key {
-	type BruteForceIter: Iterator;
+	type BruteForceIter: Iterator<Item = Self>;
 
 	fn start() -> Self::BruteForceIter;
 	fn into_brute_force_iterator(self) -> Self::BruteForceIter;
 }
 
 pub trait IntoMutationIterator: Key {
-	type MutationIter: Iterator;
+	type MutationIter: Iterator<Item = Self>;
 
 	fn into_mutation_iterator(self) -> Self::MutationIter;
 }
 
 pub trait IntoRandomIterator: Key {
-	type RandomIter: Iterator;
+	type RandomIter: Iterator<Item = Self>;
 
 	fn into_random_iterator() -> Self::RandomIter;
 }
