47628a3297cd6365b0df48a79db2f78c81508d9a
Christopher Smith <git@chrstsmth.ca>
2020-04-19 18:25:34 -0600

    working tests for caesar & vigenere

--- cipher-lib/src/cipher/caesar/tests.rs
+++ cipher-lib/src/cipher/caesar/tests.rs
@@ -1,17 +1,35 @@
 use super::*;
+use std::str::FromStr;
 
 #[test]
-fn test_encipher_wehavebeendiscoveredfleeatonce_b() {
+fn test_encipher_wehavebeendiscoveredfleeatonce() {
 	let plaintext = String::from("wehavebeendiscoveredfleeatonce");
-	let key = CaesarKey::try_from(String::from("b")).unwrap();
-	let ciphertext = Caesar::encipher(&plaintext, &key);
+	let key = CaesarKey::from_str("b").unwrap();
+	let ciphertext = Caesar::encipher(&plaintext, &key, &());
 	assert!(ciphertext == "xfibwfcffoejtdpwfsfegmffbupodf");
 }
 
 #[test]
-fn test_decipher_xfibwfcffoejtdpwfsfegmffbupodf_b() {
+fn test_decipher_wehavebeendiscoveredfleeatonce() {
 	let ciphertext = String::from("xfibwfcffoejtdpwfsfegmffbupodf");
-	let key = CaesarKey::try_from(String::from("b")).unwrap();
-	let plaintext = Caesar::decipher(&ciphertext, &key);
+	let key = CaesarKey::from_str("b").unwrap();
+	let plaintext = Caesar::decipher(&ciphertext, &key, &());
 	assert!(plaintext == "wehavebeendiscoveredfleeatonce");
+}
+
+#[test]
+fn test_encipher_thequickbrownfoxjumpsoveralazydog() {
+	let plaintext = String::from("thequickbrownfoxjumpsoveralazydog");
+	let key = CaesarKey::from_str("n").unwrap();
+	let ciphertext = Caesar::encipher(&plaintext, &key, &());
+	assert!(ciphertext == "gurdhvpxoebjasbkwhzcfbirenynmlqbt");
+}
+
+#[test]
+fn test_decipher_thequickbrownfoxjumpsoveralazydog() {
+	let ciphertext = String::from("gurdhvpxoebjasbkwhzcfbirenynmlqbt");
+	let key = CaesarKey::from_str("n").unwrap();
+	let plaintext = Caesar::decipher(&ciphertext, &key, &());
+	println!("{}", plaintext);
+	assert!(plaintext == "thequickbrownfoxjumpsoveralazydog");
 }
--- cipher-lib/src/cipher/vigenere/tests.rs
+++ cipher-lib/src/cipher/vigenere/tests.rs
@@ -1,43 +1,34 @@
-use crate::test_util::*;
-use std::fs::File;
-use std::io::*;
 use super::*;
+use std::str::FromStr;
 
 #[test]
 fn test_encipher_wehavebeendiscoveredfleeatonce_password() {
 	let plaintext = String::from("wehavebeendiscoveredfleeatonce");
-	let key = VigenereKey::try_from(String::from("password")).unwrap();
-	let ciphertext = Vigenere::encipher(&plaintext, &key);
+	let key = VigenereKey::from_str("password").unwrap();
+	let ciphertext = Vigenere::encipher(&plaintext, &key, &());
 	assert!(ciphertext == "lezsrsshtnvaoqfytrwvbzvhptgfys");
 }
 
 #[test]
 fn test_decipher_lezsrsshtnvaoqfytrwvbzvhptgfys_password() {
 	let ciphertext = String::from("lezsrsshtnvaoqfytrwvbzvhptgfys");
-	let key = VigenereKey::try_from(String::from("password")).unwrap();
-	let plaintext = Vigenere::decipher(&ciphertext, &key);
+	let key = VigenereKey::from_str("password").unwrap();
+	let plaintext = Vigenere::decipher(&ciphertext, &key, &());
 	assert!(plaintext == "wehavebeendiscoveredfleeatonce");
 }
 
 #[test]
-fn test_dictionary_lezsrsshtnvaoqfytrwvbzvhptgfys_password() {
-	let resources = cipher_tools_resources().unwrap();
-	let dictionary = resources.join("vigenere_dictionary.txt");
-	let language = resources.join("alice_language.txt");
+fn test_encipher_thequickbrownfoxjumpsoveralazydog() {
+	let plaintext = String::from("thequickbrownfoxjumpsoveralazydog");
+	let key = VigenereKey::from_str("ciphertools").unwrap();
+	let ciphertext = Vigenere::encipher(&plaintext, &key, &());
+	assert!(ciphertext == "vptxyzvypcgyvuvbanaddgxmghprsmrzy");
+}
 
-	let ciphertext = String::from("lezsrsshtnvaoqfytrwvbzvhptgfys");
-
-	let dictionary_file = File::open(dictionary).unwrap();
-	let dictionary = BufReader::new(dictionary_file)
-		.lines()
-		.map(|x| x.unwrap())
-		.map(|x| VigenereKey::try_from(x.clone()).unwrap());
-
-	let language_file = File::open(language).unwrap();
-	let language_reader = BufReader::new(language_file);
-	let lang: LanguageModel = serde_json::from_reader(language_reader).unwrap();
-
-	let candidates = Vigenere::dictionary_attack(&ciphertext, dictionary, 1, lang, Arc::new(AtomicBool::new(false)));
-	assert!(candidates[0].text == "wehavebeendiscoveredfleeatonce");
-	assert!(candidates[0].key == VigenereKey::try_from(String::from("password")).unwrap());
+#[test]
+fn test_decipher_thequickbrownfoxjumpsoveralazydog() {
+	let ciphertext = String::from("vptxyzvypcgyvuvbanaddgxmghprsmrzy");
+	let key = VigenereKey::from_str("ciphertools").unwrap();
+	let plaintext = Vigenere::decipher(&ciphertext, &key, &());
+	assert!(plaintext == "thequickbrownfoxjumpsoveralazydog");
 }
