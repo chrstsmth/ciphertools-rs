2670891ee2fc9c4312ec9d1271126e0ed470bded
Christopher Smith <git@chrstsmth.ca>
2020-05-22 00:15:08 -0600

    language_modeler

--- cipher-tools/language-modeler/src/main.rs
+++ cipher-tools/language-modeler/src/main.rs
@@ -1,32 +1,43 @@
 extern crate cipher_lib;
 extern crate serde;
 extern crate serde_json;
+extern crate clap;
 
 use std::convert::TryFrom;
 use std::fs::File;
-use std::io::prelude::*;
-
 use cipher_lib::language_model::*;
 use cipher_lib::alphabet::latin::*;
+use clap::{App, Arg, AppSettings};
+use std::io::BufReader;
+use std::io::BufRead;
 
 fn main() {
 	let mut l: LanguageModel = LanguageModel::new();
 
-	for arg in std::env::args().skip(1) {
-		let mut file = match File::open(&arg) {
-			Err(why) => panic!("couldn't open {}: {}", arg, why.to_string()),
+	let matches = App::new("Cipher Tools")
+		.setting(AppSettings::ArgRequiredElseHelp)
+		.arg(Arg::with_name("windowed")
+			.long("windowed"))
+		.arg(Arg::with_name("files")
+			.value_name("FILES")
+			.required(true))
+		.get_matches();
+
+	let files = matches.values_of("files").unwrap();
+	for file_name in files {
+		let file = match File::open(&file_name ) {
+			Err(why) => panic!("couldn't open {}: {}", file_name, why.to_string()),
 			Ok(file) => file,
 		};
 
-		let mut s = String::new();
-		match file.read_to_string(&mut s) {
-			Err(why) => panic!("couldn't open {}: {}", arg, why.to_string()),
-			_ => (),
-		}
-
-		let it = s
+		let windowed = matches.is_present("windowed");
+		let it = BufReader::new(file)
 			.lines()
 			.map(|y| {
+				let y = match y {
+					Err(why) => panic!("Failed to read {}: {}", file_name, why.to_string()),
+					Ok(y) => y
+				};
 				let mut x = y.split('\t');
 				x.next(); // word number
 				let word = String::from(x.next().unwrap());
@@ -40,7 +51,13 @@ fn main() {
 						return false;
 					}
 				}
-				return true;
+				true
+			})
+			.filter(|(_, freq)| {
+				if *freq < 50 {
+					return true //TODO
+				}
+				true
 			})
 			.map(|(word, freq)| {
 				let word: Vec<Latin> = word.chars().map(|x| Latin::try_from(x).unwrap()).collect();
@@ -48,7 +65,11 @@ fn main() {
 			});
 
 		for (word, freq) in it {
-			l.insert_word_n_times(&mut word.clone().into_iter(), freq);
+			if windowed {
+				l.windowed_insert_word_n_times(word.clone().into_iter(), freq);
+			} else {
+				l.insert_word_n_times(word.clone().into_iter(), freq);
+			}
 		}
 
 		println!("{}", serde_json::to_string(&l).unwrap());
--- resources/tools/generate-language-models.rb
+++ resources/tools/generate-language-models.rb
@@ -5,17 +5,23 @@ target_path = ARGV[1]
 language_modeler_cmd = ARGV[2]
 
 wortschatz_path = "#{src_path}/wortschatz"
-out_path = "#{target_path}/languages"
-FileUtils.mkdir_p out_path
+language_path = "#{target_path}/languages"
+out_path_windowed = "#{language_path}/windowed"
+out_path_words = "#{language_path}/words"
+FileUtils.mkdir_p out_path_windowed
+FileUtils.mkdir_p out_path_words
 
 wortschatz = Dir["#{wortschatz_path}/**/*words.txt"]
 
 wortschatz.each {|w|
 	wortschatz_name = File.basename(w, File.extname(w)) + ".lang"
 
-	language_path = "#{out_path}/#{wortschatz_name}"
-	command = "#{language_modeler_cmd} #{w} > #{language_path}"
+	commands = []
+	commands.push("#{language_modeler_cmd} #{w} > #{out_path_words}/#{wortschatz_name}")
+	commands.push("#{language_modeler_cmd} --windowed #{w} > #{out_path_windowed}/#{wortschatz_name}")
 
-	puts command
-	system(command)
+	commands.each { |c|
+		puts c
+		system(c)
+	}
 }
